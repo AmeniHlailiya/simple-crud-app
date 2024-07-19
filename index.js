93e7a85a6c861fa960455cd2f5d1321821fc61db
ameni <amenihlailiya9@gmail.com>
2024-07-19 23:31:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require("mongoose");
+
+const app = express()
+
+app.listen(3000, ()=> {
+    console.log('le serveur est éxécuté sur le port 3000');
+});
+
+app.get('/', (req,res) => {
+    res.send("hello from node API ameniiii");
+});
+
+
+mongoose
+  .connect(
+    "mongodb://localhost:27017/"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
